35c83abc0a63d5fa583fcc643895881fc884218c
Jai Priyadarashi <jaipriyadarashi@MacBook-Pro.local>
2020-12-30 21:41:29 -0500

    test case added for pricing microservice

--- pricing-service/src/test/java/com/udacity/pricing/PricingServiceApplicationTests.java
+++ pricing-service/src/test/java/com/udacity/pricing/PricingServiceApplicationTests.java
@@ -1,16 +1,37 @@
 package com.udacity.pricing;
 
+import com.udacity.pricing.entity.Price;
+import com.udacity.pricing.repository.PriceRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.math.BigDecimal;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class PricingServiceApplicationTests {
 
+
+	@Autowired
+	private PriceRepository priceRepository;
+
+	@Before
+	public void initDBRec(){
+		Price price = new Price(1L, "USD", new BigDecimal(10000.00));
+		priceRepository.save(price);
+	}
 	@Test
 	public void contextLoads() {
 	}
 
+	@Test
+	public void getPricesByID() throws Exception{
+		BigDecimal price = priceRepository.findById(1L).get().getPrice();
+		Assertions.assertThat(price).isEqualTo(price);
+   	}
 }
